b96964c0a667d9dfeccf11ffc4f665a6b09ce24d
evelan <evelan3094@gmail.com>
2017-12-02 15:05:42 +0100

    add reseting scene

---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.X)){
+			resetScene();
+		}
+         
+	}
+
+	void resetScene(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
