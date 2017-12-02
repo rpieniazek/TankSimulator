c05a7de08a6cfc3aaf775cf4c728aff26fc00378
rafalpieniazek <rkpieniazek@gmail.com>
2017-12-02 13:10:20 +0100

    Shooting script, material for bullter added

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public float speed = 20;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation)as Rigidbody;
+			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0,speed));
+		}
+	}
+}
