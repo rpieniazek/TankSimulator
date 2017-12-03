a7d1e2e294e027c7be46d2a55913c246c7912c54
unknown <tan.dawid@gmail.com>
2017-12-03 23:33:01 +0100

    sterowanie + stabilizacja lufy

---
+++ Assets/Scripts/BarrelStabilization.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelStabilization : MonoBehaviour {
+    public float Kp = 200;
+    public float Ki = 1;
+    public float Kd = 0.1f;
+    private float P, I, D;
+    private float prevError;
+
+    private Vector3 targetVector;
+    public Transform turret;
+    public Transform barrel;
+    private bool aimMode = false;
+    
+
+    // Use this for initialization
+    void Start () {
+        targetVector = barrel.up.normalized;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!aimMode)
+        {
+            float angle = Vector3.Angle(turret.forward, barrel.up);
+            if(angle <= 105 && angle >=85)
+            {
+                barrel.localRotation *= Quaternion.Euler(Input.GetAxis("Elevation"), 0, 0);
+            } else if (angle > 105 && Input.GetKey("w"))
+            {
+                barrel.localRotation *= Quaternion.Euler(1, 0, 0);
+            } else if (angle < 85 && Input.GetKey("s"))
+            {
+                barrel.localRotation *= Quaternion.Euler(-1, 0, 0);
+            }
+            targetVector = barrel.up.normalized;
+        }
+        else
+        {
+            float dt = Time.fixedDeltaTime;
+            float targetAngle = Vector3.Angle(targetVector, Vector3.up);
+            float currentAngle = Vector3.Angle(barrel.up.normalized, Vector3.up);
+            float angleError = targetAngle - currentAngle;
+            if (angleError < 0.7 && angleError > -0.7)
+            {
+                barrel.Rotate(0, 0 ,0);
+            }
+            else
+            {
+                barrel.Rotate(-Pid(angleError, dt)*dt*dt, 0, 0);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            aimMode = !aimMode;
+        }
+    }
+
+    public float Pid(float currentError, float deltaTime)
+    {
+        P = currentError;
+        I += P * deltaTime;
+        D = (P - prevError) / deltaTime;
+        prevError = currentError;
+
+        return P * Kp + I * Ki + D * Kd;
+    }
+}
---
+++ Assets/Scripts/TurretStabilization.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretStabilization : MonoBehaviour {
+    public float Kp = 200;
+    public float Ki = 1;
+    public float Kd = 0.1f;
+    private float P, I, D;
+    private float prevError;
+    private float targetAngle;
+    public Transform turret;
+    private bool aimMode = false;
+    private Vector3 targetVector;
+    private Vector3 targetVector2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(!aimMode)
+        {
+            turret.localRotation *= Quaternion.Euler(0, 0, Input.GetAxis("Yaw"));
+            targetVector = Vector3.ProjectOnPlane(turret.up, Vector3.right);
+            targetVector2 = Vector3.ProjectOnPlane(turret.up, Vector3.forward);
+            Debug.DrawLine(Vector3.zero, targetVector, Color.red);
+            Debug.DrawLine(Vector3.zero, targetVector2, Color.red);
+
+
+            //float angle = Vector3.Angle(turret.up, Vector3.forward);
+            //float sign = Mathf.Sign(Vector3.Dot(turret.right, Vector3.forward));
+            //float finalAngle = sign * angle;
+            //targetAngle = finalAngle + 180;
+            //print(targetAngle);
+        }
+        else
+        {
+            Vector3 diffrenceVector = Vector3.ProjectOnPlane(turret.up, Vector3.right) - targetVector;
+            float angleDiff = Vector3.Angle(diffrenceVector, targetVector);
+            print(angleDiff);
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            aimMode = !aimMode;
+        }
+
+    }
+
+    public float Pid(float currentError, float deltaTime)
+    {
+        P = currentError;
+        I += P * deltaTime;
+        D = (P - prevError) / deltaTime;
+        prevError = currentError;
+
+        return P * Kp + I * Ki + D * Kd;
+    }
+
+}
