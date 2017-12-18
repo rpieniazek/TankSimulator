d785b9e4162a579f5f3b4e3645b776671a13c401
rafalpieniazek <rkpieniazek@gmail.com>
2017-12-18 17:14:06 +0100

    wall on main scene, tank on main scene

---
+++ Assets/DestructibleWallGenerator/DWG/Scripts/DWGDestroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DWGDestroyer : MonoBehaviour {
+
+	public float radius = 2;
+	public float force = 50f;
+	
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Destructible") {
+			ExplodeForce();
+
+		}
+	}
+	
+	// Explode force by radius only if a destructible tag is found
+	void ExplodeForce(){	
+		Debug.Log ("Explosion!!!");
+		Vector3 explodePos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explodePos, radius); 
+		foreach (Collider hit in colliders){
+			if(hit.GetComponent<Collider>().tag == "Destructible"){
+				if(hit.GetComponent<Rigidbody>()){
+					hit.GetComponent<Rigidbody>().isKinematic = false; 
+					hit.GetComponent<Rigidbody>().AddExplosionForce(force, explodePos,radius);
+				}
+			}
+		}
+		//Destroy (gameObject);
+	}
+}
